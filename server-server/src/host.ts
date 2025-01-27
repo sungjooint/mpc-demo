ef0388a7976285d262d12a1f94854f4975e5e5c2
cedoor <me@cedoor.dev>
2025-01-27 16:16:46 +0000

    docs: add some comments to the code

--- server-server/src/host.ts
+++ server-server/src/host.ts
@@ -2,24 +2,31 @@ import assert from 'assert';
 import inquirer from 'inquirer';
 import { Session } from 'mpc-framework';
 import { WebSocketServer } from 'ws';
+import { PORT } from './utils/constants';
 import generateProtocol from './utils/generateProtocol';
 
-export const PORT = 8080;
+// This script is the host server and it is supposed to impersonate Bob in the MPC.
 
+// Create a WebSocket server that listens on the specified PORT.
 const wss = new WebSocketServer({ port: PORT });
 
 console.info(`Listening on port ${PORT}...\n`);
 
+// Listen for incoming WebSocket connections.
 wss.on('connection', async ws => {
   ws.on('error', console.error);
 
+  // Generate the protocol with mpc-framework.
   const protocol = await generateProtocol('./src/circuit/main.ts');
 
+  // Set the session variable to manage the MPC between two
+  // parties: Bob (this server) and Alice.
   let session: Session;
 
+  // Handle incoming messages from Alice.
   ws.on('message', async (msg: Buffer) => {
     if (!session) {
-      // Bob's circuit input.
+      // Prompt the user for input to provide Bob's circuit input.
       const { number } = await inquirer.prompt({
         type: 'input',
         name: 'number',
@@ -29,9 +36,11 @@ wss.on('connection', async ws => {
       session = protocol.join('bob', { b: Number(number) }, (to, msg) => {
         assert(to === 'alice', 'Unexpected party');
 
+        // Send the MPC message to Alice.
         ws.send(msg);
       });
 
+      // Handle the MPC output to log the result.
       session.output().then(({ main }) => {
         console.info(`\nThe largest number is: ${main}\n`);
 
@@ -39,6 +48,7 @@ wss.on('connection', async ws => {
       });
     }
 
+    // Process incoming MPC messages for the session, assuming they are from Alice.
     session.handleMessage('alice', new Uint8Array(msg));
   });
 });
